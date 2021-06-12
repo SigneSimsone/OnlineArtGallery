44d7b03caa3ee908ee9e5e0e26e586e23f302006
signe <signe@DESKTOP-8PR1T1V>
2021-06-12 12:28:51 +0300

    open one artwork, artist - view; favorite artists, artworks pages (unfavorite and open artist/artwork in those pages)

--- OnlineArtGallery.Web/Controllers/FavoritesController.cs
+++ OnlineArtGallery.Web/Controllers/FavoritesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineArtGallery.Web.Data.Managers;
 using OnlineArtGallery.Web.Models;
+using System;
 
 namespace OnlineArtGallery.Web.Controllers
 {
@@ -14,12 +15,12 @@ namespace OnlineArtGallery.Web.Controllers
         public FavoritesController(ArtworkDataManager artworkDataManager, UserManager<UserModel> userManager, ArtistDataManager artistDataManager)
         {
             _artworkDataManager = artworkDataManager;
-            _userManager = userManager;
             _artistDataManager = artistDataManager;
+            _userManager = userManager;
         }
 
         [Route("favorite-artists")]
-        public IActionResult Index()
+        public IActionResult FavoriteArtists()
         {
             var user = _userManager.GetUserAsync(User).Result;
             ArtistViewModel viewModel = new ArtistViewModel();
@@ -28,6 +29,53 @@ namespace OnlineArtGallery.Web.Controllers
             viewModel.UserId = user.Id;
 
             return View("FavoriteArtists", viewModel);
+        }
+
+        [Route("favorite-artworks")]
+        public IActionResult FavoriteArtworks()
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+            ArtworkViewModel viewModel = new ArtworkViewModel();
+            var artworks = _artworkDataManager.GetFavoriteArtworks(user);
+            viewModel.Artworks = artworks;
+            viewModel.UserId = user.Id;
+
+            /*foreach (var artwork in viewModel.Artworks){
+                if (artwork.Users.Any(x => x.Id == viewModel.UserId)){}}*/
+
+            return View("FavoriteArtworks", viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult UnfavoriteArtist(Guid id)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+
+            _artistDataManager.UnFavoriteArtist(id, user);
+
+            return RedirectToAction(nameof(FavoriteArtists));
+        }
+
+        [HttpPost]
+        public IActionResult UnfavoriteArtwork(Guid id)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+
+            _artworkDataManager.UnFavoriteArtwork(id, user);
+
+            return RedirectToAction(nameof(FavoriteArtworks));
+        }
+
+        [HttpGet]
+        public IActionResult OpenArtist(Guid Id)
+        {
+            return RedirectToAction("OpenArtist", "Artist", new { ArtistId = Id });
+        }
+
+        [HttpGet]
+        public IActionResult OpenArtwork(Guid Id)
+        {
+            return RedirectToAction("OpenArtwork", "Artwork", new { ArtworkId = Id });
         }
     }
 }
