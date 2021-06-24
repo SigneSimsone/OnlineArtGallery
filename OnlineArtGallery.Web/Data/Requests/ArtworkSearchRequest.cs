316dc6802ad924fee6cf7aef5021a323fc2dabd5
signe <signe@DESKTOP-8PR1T1V>
2021-06-24 21:37:26 +0300

    artwork search request

---
+++ OnlineArtGallery.Web/Data/Requests/ArtworkSearchRequest.cs
@@ -0,0 +1,19 @@
+﻿using OnlineArtGallery.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineArtGallery.Web.Data.Requests
+{
+    public class ArtworkSearchRequest
+    {
+        public string Title { get; set; }
+        public ArtistModel Artist { get; set; }
+        public StyleModel Style { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public float Price { get; set; }
+        public int Availability { get; set; }
+    }
+}
