9a5fedafae4abcd441f1b1a66247454904021b42
signe <signe@DESKTOP-8PR1T1V>
2021-06-25 15:41:27 +0300

    facebook login; added more fields for registering;

---
+++ OnlineArtGallery.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineArtGallery.Web.Data;
+using OnlineArtGallery.Web.Models;
+
+[assembly: HostingStartup(typeof(OnlineArtGallery.Web.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineArtGallery.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
