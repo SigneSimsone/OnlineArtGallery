cacfe8e6fa8f226781dbb5c62b10fd90c615cfbb
Linda Sticere <ls20113@edu.lu.lv>
2021-06-26 10:29:05 +0300

    created audit functionality

--- OnlineArtGallery.Web/Controllers/AuditController.cs
+++ OnlineArtGallery.Web/Controllers/AuditController.cs
@@ -9,5 +9,25 @@ using System.Threading.Tasks;
 
 namespace OnlineArtGallery.Web.Controllers
 {
-    
+    [Authorize(Roles = "Admin")]
+    public class AuditController : Controller
+    {
+        private readonly AuditDataManager _auditDataManager;
+
+        public AuditController(AuditDataManager auditDataManager)
+        {
+            _auditDataManager = auditDataManager;
+        }
+
+        public IActionResult Index()
+        {
+            AuditModel[] audits = _auditDataManager.GetAudits();
+
+            AuditViewModel viewModel = new AuditViewModel();
+            viewModel.Audits = audits;
+
+            return View(viewModel);
+        }
+    }
+
 }
--- OnlineArtGallery.Web/Data/Managers/AuditDataManager.cs
+++ OnlineArtGallery.Web/Data/Managers/AuditDataManager.cs
@@ -10,6 +10,32 @@ namespace OnlineArtGallery.Web.Data.Managers
 {
     public class AuditDataManager : Controller
     {
-        
+        private readonly ApplicationDbContext _dbContext;
+        public AuditDataManager(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public AuditModel[] GetAudits()
+        {
+            var result = _dbContext
+                .Audits
+                .Include(x => x.User)
+                .ToArray();
+
+            return result;
+        }
+        internal void AddAuditRecord(UserModel user, string action)
+        {
+            var record = new AuditModel()
+            {
+                DateTime = DateTime.UtcNow,
+                Action = action,
+                User = user
+            };
+
+            _dbContext.Audits.Add(record);
+            _dbContext.SaveChanges();
+        }
+
     }
 }
